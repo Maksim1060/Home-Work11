2ca4e9256bcd6521b84e11e1cec7ca571117b7b4
Maksim <maks.kartashov.93@inbox.ru>
2023-07-05 12:56:56 +0200

    Дз11

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void printYear(int year) {
-        System.out.println(year + " Год-високосный год");
+    public static void checkLapYaer(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " Год являеется високосным");
+        } else {
+            System.out.println(year + " Год -невисокосный год");
+        }
+
     }
 
-    public static void printClietOS(int clientOS, int clientDeviceYear) {
-        System.out.println("установите облегченную версию приложения");
+    public static void printClietOS(int clientOS, int year) {
+        int borderYear = 2015;
+        if (clientOS == 0 && year < borderYear) {
+            System.out.println("Установите облегченую версию приложения для IOS по ссылке");
+        } else if (clientOS == 0 && year >= borderYear) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 1 && year < borderYear) {
+            System.out.println("Установите облегченную версию для Android по ссылке ");
+        } else if (clientOS == 1 && year >= borderYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Не удалось определить версию");
+        }
+    }
+    public static void printDeliveyInfo(int diliveryDistance){
+        int diliveryDays = diliveryInfo(diliveryDistance);
+        if (diliveryDays == -1) {
+            System.out.println("Доставки нет");
+        }else {
+            System.out.println("Потребуется дней "+diliveryDays);
+        }
+    }
+
+    public static int diliveryInfo(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
+
     }
 
 
@@ -17,56 +56,37 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         int year = 2020;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            printYear(year);
-        } else {
-            System.out.println(year + " Год -невисокосный год");
-        }
+        int year2 = 2022;
+        checkLapYaer(year);
+        checkLapYaer(year2);
+
     }
+
 
     public static void task2() {
         System.out.println("Задача 2 ");
-        int clientOS = 0;
-        int iOS = 1;
-        int android = 0;
-        int clientDeviceYear = 2014;
-        if (clientOS == android && clientDeviceYear < 2015) {
-            printClietOS(clientOS, clientDeviceYear);
-        } else if (clientOS == iOS && clientDeviceYear < 2015) {
-            printClietOS(clientOS, clientDeviceYear);
-        } else {
-            System.out.println("Установите приложение по ссылке");
-        }
+        int year = LocalDate.now().getYear();
+        int clientOS = 1;
+        printClietOS(clientOS, year);
     }
+
 
     public static void task3() {
         System.out.println("Задача 3");
-    }
+        int deliveryDistance = 40;
+        printDeliveyInfo( 40);
 
-    public static void printDeliveryInfo(int dieiveryDistance) {
-        int deliveryDays = getDeliveryDayas(dieiveryDistance);
-        if (deliveryDays == -1) {
-            System.out.println("Доставки нет ");
-        } else {
-            System.out.println();
 
         }
     }
 
-    public static int getDeliveryDayas ( int dieiveryDistance){
-        int deliveryDistance=1;
-        if (dieiveryDistance < 20) {
-            return 1;
-        } else if (dieiveryDistance <= 60) {
-            return 2;
-        } else if (dieiveryDistance <= 100) {
-            return 3;
-        } else {
-            return -1;
-        }
-        printDeliveryInfo(3);
 
-    }
-}
+
+
+
+
+
+
+
 
 
